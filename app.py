87224b853c71f39d4d57feadc9205fdb788aefc3
supakitpao <supakit.pao@gmail.com>
2022-04-20 23:20:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import sys
-print("Python version")
-print (sys.version)
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
